0047a6e5f2a9191f1d50d6db89c24a1a2513067c
Ash20 <Ash20@CAMILO>
2024-08-09 10:50:45 -0500

    Se realiza commit de iniciar

--- CajerePOOLynx/Program.cs
+++ CajerePOOLynx/Program.cs
@@ -7,5 +7,19 @@ namespace CajereLynx
         public decimal _balance = 1000.00m;
         public string _username = "admin";
         public string _password = "1234";
+
+        public void Run()
+        {
+            Console.WriteLine("Bienvenido al Cajero Automático");
+            if (Login())
+            {
+                Console.WriteLine("Inicio de sesión exitoso");
+                MainMenu();
+            }
+            else
+            {
+                Console.WriteLine("Inicio de sesión fallido");
+            }
+        }
     }
 }
