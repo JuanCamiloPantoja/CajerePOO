f7a07acd31bb5c88d0b4792a730465ab6cca9633
Ash20 <Ash20@CAMILO>
2024-08-09 11:37:30 -0500

    Se corrige rama DEV

--- CajerePOOLynx/Program.cs
+++ CajerePOOLynx/Program.cs
@@ -4,6 +4,11 @@ namespace CajereLynx
 {
     public class CajereLynx
     {
+        static void Main(string[] args)
+        {
+            CajereLynx atm = new CajereLynx();
+            atm.Run();
+        }
         public decimal _balance = 1000.00m;
         public string _username = "admin";
         public string _password = "1234";
